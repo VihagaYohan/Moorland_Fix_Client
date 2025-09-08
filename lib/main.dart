e04ccd38632924cf1fc47adb73574573a26606e7
Vihaga Yohan <vihagayohan94@gmail.com>
2025-09-08 10:25:45 +0100

    Added - text button theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 // theme
-import 'package:moorland_fix/app/theme/light_theme.dart';
-import 'package:moorland_fix/app/theme/dark_theme.dart';
+import 'package:moorland_fix/app/theme/index.dart';
 
 void main() {
   runApp(const MyApp());
@@ -51,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            TextButton(onPressed: () {}, child: Text("button")),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
