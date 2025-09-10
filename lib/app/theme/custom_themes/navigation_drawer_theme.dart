9d74e69de1b819a7ebb044124b098cf83c30ffa3
Vihaga Yohan <vihagayohan94@gmail.com>
2025-09-10 10:14:21 +0100

    Changed - app colors in drawer navigation

--- lib/app/theme/custom_themes/navigation_drawer_theme.dart
+++ lib/app/theme/custom_themes/navigation_drawer_theme.dart
@@ -1,44 +1,45 @@
 import 'package:flutter/material.dart';
-
-// theme
-import 'package:moorland_fix/app/theme/index.dart';
-
 // utils
 import 'package:moorland_fix/app/shared/index.dart';
+// theme
+import 'package:moorland_fix/app/theme/index.dart';
 
 class TNavigationDrawer {
   TNavigationDrawer._();
 
   static var lightNavigationDrawer = NavigationDrawerThemeData(
-    backgroundColor: AppColors.lightSurface,
+    surfaceTintColor: AppColors.lightSurface,
+    backgroundColor: AppColors.lightBackground,
     elevation: 5,
     indicatorColor: AppColors.primary.withValues(alpha: 0.5),
-    surfaceTintColor: AppColors.primary,
-    labelTextStyle: WidgetStateProperty.fromMap(<WidgetStatesConstraint, TextStyle>{
-     WidgetState.selected: TextStyle(
-       color: AppColors.primary,
-       fontSize: 16,
-       fontFamily: Constants.fontFamilyPoppins
-     ),
-      WidgetState.any: TextStyle(
-        color: Colors.grey,
-        fontSize: 16,
-        fontFamily: Constants.fontFamilyPoppins
-      )
-    })
+    // surfaceTintColor: AppColors.primary,
+    labelTextStyle:
+        WidgetStateProperty.fromMap(<WidgetStatesConstraint, TextStyle>{
+          WidgetState.selected: TextStyle(
+            color: AppColors.primary,
+            fontSize: 16,
+            fontFamily: Constants.fontFamilyPoppins,
+          ),
+          WidgetState.any: TextStyle(
+            color: Colors.grey,
+            fontSize: 16,
+            fontFamily: Constants.fontFamilyPoppins,
+          ),
+        }),
   );
 
   static var darkNavigationDrawer = NavigationDrawerThemeData(
+    surfaceTintColor: AppColors.darkSurface,
     backgroundColor: AppColors.darkBackground,
     elevation: 5,
     indicatorColor: AppColors.primary.withValues(alpha: 0.3),
-    surfaceTintColor: AppColors.primary,
-    labelTextStyle: WidgetStateProperty.fromMap(<WidgetStatesConstraint, TextStyle>{
-      WidgetState.selected: TextStyle(
-        color: AppColors.darkPrimary,
-        fontSize: 16,
-        fontFamily: Constants.fontFamilyPoppins
-      )
-    }),
+    labelTextStyle:
+        WidgetStateProperty.fromMap(<WidgetStatesConstraint, TextStyle>{
+          WidgetState.selected: TextStyle(
+            color: AppColors.darkPrimary,
+            fontSize: 16,
+            fontFamily: Constants.fontFamilyPoppins,
+          ),
+        }),
   );
 }
